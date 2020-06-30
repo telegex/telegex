923b6ae125b5127229252bdd946a64cd2d711e83
Hentioe <me@bluerain.io>
2020-07-01 02:22:28 +0800

    Fix #14

--- lib/telegex.ex
+++ lib/telegex.ex
@@ -1009,6 +1009,25 @@ defmodule Telegex do
     :boolean
   )
 
+  @doc """
+  Informs a user that some of the Telegram Passport elements they provided contains errors.
+  The user will not be able to re-submit their Passport to you until the errors are fixed (the contents of
+  the field for which you returned the error must change). Returns True on success.
+
+  Use this if the data submitted by the user doesn't satisfy the standards your service requires for any reason.
+  For example, if a birthday date seems invalid, a submitted document is blurry,
+  a scan shows evidence of tampering, etc. Supply some details in the error message to make sure the user
+  knows how to correct the issues.
+  """
+  method(
+    "setPassportDataErrors",
+    [
+      {:user_id, :integer},
+      {:errors, [PassportElementError]}
+    ],
+    :boolean
+  )
+
   # Convert the accumulated attribute value to `map`.
   # **Note**: This attribute needs to be defined after all `Telex.DSL.method/3` calls.
   @include_attachment_methods_mapping @include_attachment_methods_meta
