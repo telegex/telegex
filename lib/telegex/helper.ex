522f14b2597b0ff12fc642e183d4125eba0e336c
Hentioe <meow.i5.br@gmail.com>
2023-06-07 11:58:19 +0800

    Add comments

--- lib/telegex/helper.ex
+++ lib/telegex/helper.ex
@@ -9,8 +9,10 @@ defmodule Telegex.Helper do
     Enum.map(map, fn item -> typedmap(item, type) end)
   end
 
-  # 暂时不处理联合类型转换
-  # 联合类型需要根据关键内容或结构来确定具体类型
+  # 暂时不处理联合类型返回值转换，因为联合类型需要根据关键内容或结构来确定具体类型。
+  # 一些联合类型返回值的例子：
+  # - 更新消息相关的方法会返回 True 或 Message
+  # - getChatAdministrators 方法或返回 ChatMember 的列表，而 ChatMember 自身就是联合类型
   def typedmap(map, %UnionType{types: _types}) do
     map
   end
