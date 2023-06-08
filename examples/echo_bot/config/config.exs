2be3c80b25fa135c5d641aa49c98b7211a4a8bf7
Hentioe <meow.i5.br@gmail.com>
2023-06-08 14:44:17 +0800

    Update examples

--- examples/echo_bot/config/config.exs
+++ examples/echo_bot/config/config.exs
@@ -1,7 +1,5 @@
 import Config
 
-config :telegex, Telegex.Caller,
-  adapter: Telegex.Caller.FinchAdapter,
-  options: [recv_timeout: 5 * 1000]
+config :telegex, caller_adapter: {Finch, [receive_timeout: 5 * 1000]}
 
 import_config "#{config_env()}.exs"
