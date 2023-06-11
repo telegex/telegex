9f41bc584ed97e1178c35b24d943cb4b7c191d79
Hentioe <meow.i5.br@gmail.com>
2023-06-11 16:30:42 +0800

    Add safe markdown fun

--- lib/telegex/tools.ex
+++ lib/telegex/tools.ex
@@ -1,13 +1,49 @@
 defmodule Telegex.Tools do
   @moduledoc "Some utility functions."
 
-  @spec safe_html(String.t()) :: binary
+  @type markdown_from :: :text | :inline_link | :pre | :code | :pre_or_code
+
+  @spec safe_html(String.t()) :: String.t()
   def safe_html(html) do
     html
     |> String.replace("&", "&amp;")
     |> String.replace("<", "&lt;")
     |> String.replace(">", "&gt;")
     |> String.replace("\"", "&quot;")
+  end
+
+  @doc """
+  Generate safe Markdown content.
+
+  ## Examples
+      iex> Telegex.Tools.safe_markdown("'_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'")
+      ~S"'\\_', '\\*', '\\[', '\\]', '\\(', '\\)', '\\~', '\\`', '\\>', '\\#', '\\+', '\\-', '\\=', '\\|', '\\{', '\\}', '\\.', '\\!'"
+      iex> Telegex.Tools.safe_markdown(~S{https://t.me\\)}, :inline_link)
+      ~S|https://t.me\\\\\\)|
+      iex> Telegex.Tools.safe_markdown(~S{IO.puts `Hello`}, :pre_or_code)
+      ~S|IO.puts \\`Hello\\`|
+  """
+  @spec safe_markdown(String.t(), markdown_from) :: String.t()
+  def safe_markdown(conetnt, from \\ :text),
+    do: _safe_markdown(conetnt, from)
+
+  defp _safe_markdown(conetnt, :text) do
+    String.replace(
+      conetnt,
+      ~r/(\_|\*|\[|\]|\(|\)|\~|\`|\>|\#|\+|\-|\=|\||\{|\}|\.|\!)/,
+      "\\\\\\g{1}"
+    )
+  end
+
+  defp _safe_markdown(conetnt, :inline_link) do
+    conetnt |> String.replace("\\", "\\\\") |> String.replace(")", "\\)")
+  end
+
+  defp _safe_markdown(conetnt, :pre), do: _safe_markdown(conetnt, :pre_or_code)
+  defp _safe_markdown(conetnt, :code), do: _safe_markdown(conetnt, :pre_or_code)
+
+  defp _safe_markdown(conetnt, :pre_or_code) do
+    conetnt |> String.replace("\\", "\\\\") |> String.replace("`", "\\`")
   end
 
   @doc """
---
+++ test/telegex/tools_test.exs
@@ -0,0 +1,4 @@
+defmodule Telegex.ToolsTest do
+  use ExUnit.Case
+  doctest Telegex.Tools
+end
