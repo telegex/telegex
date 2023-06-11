c1c6fdc9a6b82d47c699f2253f85967c6e374caa
Hentioe <meow.i5.br@gmail.com>
2023-06-11 08:59:58 +0800

    Add gen secret token fun

--- lib/telegex/tools.ex
+++ lib/telegex/tools.ex
@@ -9,4 +9,15 @@ defmodule Telegex.Tools do
     |> String.replace(">", "&gt;")
     |> String.replace("\"", "&quot;")
   end
+
+  @doc """
+  Generate a secret token string.
+
+  By default, it generates 16 bytes of data and converts it to a 16-digit hexadecimal string (32 characters long).
+  The `bytes_count` parameter can be passed to control the length of the generated string.
+  """
+  @spec gen_secret_token(non_neg_integer) :: String.t()
+  def gen_secret_token(bytes_count \\ 16) do
+    bytes_count |> :crypto.strong_rand_bytes() |> Base.encode16()
+  end
 end
