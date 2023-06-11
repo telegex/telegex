4aca7445d3e2620d70a68a1e2f8a9a8662ff5ac1
Hentioe <meow.i5.br@gmail.com>
2023-06-11 16:29:58 +0800

    Remove req tests

--- test/telegex/caller/adapter/adapter_test.exs
+++ test/telegex/caller/adapter/adapter_test.exs
@@ -26,23 +26,6 @@ defmodule Telegex.Caller.AdapterTest do
     assert match?({:error, %Telegex.RequestError{reason: :timeout}}, r)
   end
 
-  test "Adapter.Req ok" do
-    Application.put_env(:telegex, :caller_adapter, Req)
-
-    r = call("getMe")
-
-    assert match?({:ok, %{is_bot: true}}, r)
-  end
-
-  test "Adapter.Req error" do
-    # 传递选项测试超时错误
-    Application.put_env(:telegex, :caller_adapter, {Req, [connect_options: [timeout: 1]]})
-
-    r = call("getMe")
-
-    assert match?({:error, %Telegex.RequestError{reason: :timeout}}, r)
-  end
-
   test "Adapter.Finch ok" do
     Application.put_env(:telegex, :caller_adapter, Finch)
 
