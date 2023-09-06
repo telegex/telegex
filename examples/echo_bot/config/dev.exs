a2dce73d09a24089e71036437c488891df0b01eb
Hentioe <meow.i5.br@gmail.com>
2023-09-07 07:02:56 +0800

    Update echo bot config

--- examples/echo_bot/config/dev.exs
+++ examples/echo_bot/config/dev.exs
@@ -1,3 +1,9 @@
 import Config
 
+config :echo_bot, work_mode: :polling
+
+config :telegex,
+  hook_adapter: Bandit,
+  caller_adapter: {Finch, [receive_timeout: 15 * 1000]}
+
 import_config "dev.secret.exs"
