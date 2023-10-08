3caaf7d17f962d31569cb4b8c7e555a5e8930bc1
Hentioe <meow.i5.br@gmail.com>
2023-10-09 02:30:47 +0800

    Update deps

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Telegex.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
       {:httpoison, "~> 2.1", optional: true, only: [:dev, :test]},
       {:finch, "~> 0.16.0", optional: true, only: [:dev, :test]},
       {:multipart, "~> 0.4.0", optional: true, only: [:dev, :test]},
