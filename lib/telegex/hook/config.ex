7d7499c8e8dcccff836fb1b74c9d8ed1173b7fb6
Hentioe <meow.i5.br@gmail.com>
2023-10-09 17:01:16 +0800

    Add webhook config documents

--- lib/telegex/hook/config.ex
+++ lib/telegex/hook/config.ex
@@ -1,5 +1,13 @@
 defmodule Telegex.Hook.Config do
-  @moduledoc false
+  @moduledoc """
+  Configure webhook mode.
+
+  ## Field descriptions
+
+    * `server_port` - Port for the webhook server. Defaults to `4000`.
+    * `secret_token` - The `secret_token` parameter of the [`setWebhook`](https://core.telegram.org/bots/api#setwebhook) method.
+    Use `Telegex.Tools.gen_secret_token/1` to generate a reliable token.
+  """
 
   use TypedStruct
 
