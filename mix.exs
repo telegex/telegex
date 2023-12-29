1c00648c530e9c7438f6ae47533a0e88ba0bd409
Hentioe <meow.i5.br@gmail.com>
2023-12-29 21:45:08 +0800

    Start v1.4.1-dev

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Telegex.MixProject do
   use Mix.Project
 
-  @version "1.4.0"
+  @version "1.4.1-dev"
   @description "A Telegram bot framework, with its client-side based on data and code generation, boasts unparalleled adaptation speed and correctness for new versions."
 
   def project do
