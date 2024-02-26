6eb2e3e16c82b295fcf11cbd5b8af5b535d9f7ef
Tw <tw19881113@gmail.com>
2024-02-26 15:19:38 +0800

    use `with` to implement pipeline macro

--- lib/telegex/chain/handler.ex
+++ lib/telegex/chain/handler.ex
@@ -1,19 +1,6 @@
 defmodule Telegex.Chain.Handler do
   @moduledoc false
 
-  # 以管道的形式调用多个 chain
-  def call(chains, update, context \\ %{}) when is_list(chains) and is_map(context) do
-    call_chain = fn chain, {_state, context} ->
-      case chain.call(update, context) do
-        {:ok, context} -> {:cont, {:ok, context}}
-        {:stop, context} -> {:halt, {:stop, context}}
-        {:done, context} -> {:halt, {:done, context}}
-      end
-    end
-
-    Enum.reduce_while(chains, {:init, context}, call_chain)
-  end
-
   defmacro __using__(_) do
     quote do
       import unquote(__MODULE__), only: [pipeline: 1]
@@ -23,15 +10,36 @@ defmodule Telegex.Chain.Handler do
   end
 
   defmacro pipeline(modules) do
-    quote do
-      def __chains__ do
-        unquote(modules)
-      end
+    clauses_ast =
+      modules
+      |> Enum.map(&build_with_clause_ast/1)
+      |> Enum.to_list()
 
+    quote do
       @spec call(Telegex.Type.Update.t(), Telegex.Chain.context()) :: Telegex.Chain.result()
       def call(update, context) do
-        unquote(__MODULE__).call(__MODULE__.__chains__(), update, context)
+        with unquote_splicing(clauses_ast) do
+          {:ok, context}
+        else
+          {:stop, _} = stop_r ->
+            stop_r
+
+          {:done, _} = done_r ->
+            done_r
+
+          {state, context} when is_map(context) ->
+            raise "Unknown chain state: #{inspect(state)}"
+
+          other ->
+            raise "Unknown chain result: #{inspect(other)}"
+        end
       end
+    end
+  end
+
+  defp build_with_clause_ast(module) do
+    quote do
+      {:ok, context} <- unquote(module).call(update, context)
     end
   end
 end
